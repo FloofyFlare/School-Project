2113d57144096ed159b86ff9ceca1e91e0c4d943
Raymond Flowers IV <85596143+FloofyFlare@users.noreply.github.com>
2022-05-22 15:05:11 -0400

    Update database.cpp

--- database.cpp
+++ database.cpp
@@ -4,68 +4,81 @@
 
 using namespace std;
 
+int iterations = 0;
+
 struct Person
 {
     string passions, skills, intrests;
     int effort;
-    struct Person *next;
+    struct Person* next;
 
-    Person(string passion, string skill,string intrest, int work){
+    Person(string passion, string skill, string intrest, int work) {
         passions = passion;
         skills = skill;
         intrests = intrest;
         effort = work;
     }
-
-     // returns a list of possible team members for this Person struct
-     vector<Person> findTeam(Person person[], int length){
-        vector<Person> team;
-        for (int i = 0; i < length; i++){
-            if (person[i].effort == effort){
-                team.push_back(person[i]); 
-            }
-
-        }
-        return team;
-    }
 };
 
 struct Person* head;
+        void DatabaseEnter(int i) {
+        string passions, skills, intrests;
+        int effort;
+        cout << "Enter your hobbies, skills, work, and intrests" << endl;
+        cout << endl << "hobbies:";
+        getline(cin, passions);
+        cout << endl << "skills:";
+        getline(cin, skills);
+        cout << endl << "intrest:";
+        getline(cin, intrests);
+        cout << endl << "work:";
+        cin >> effort;
 
-void DatabaseEnter(int i) {
-    string passions, skills, intrests;
-    int effort;
-    printf("Enter your hobbies, skills, work, and intrests");
-    cout << endl << "hobbies:";
-    getline(cin,passions);
-    cout << endl << "skills:";
-    getline(cin,skills);
-    cout << endl << "intrest:";
-    getline(cin,intrests);
-    cout << endl << "work:";
-    getline(cin,effort);   
-    
-    Person* temp = new Person*(passions, skills, intrests, effort);
-    
-    if (i == 0)
-    head = temp;
-    else{
-       struct Person* temp2 = head;
-     for(int i = 0; i < i - 1; i++){   
-       temp2 = temp2->next;
+        Person* temp = new Person(passions, skills, intrests, effort);
+
+        if (i == 0) {
+            head = temp;
+            temp->next = NULL;
+            iterations++;
+        }
+        else {
+            struct Person* temp2 = head;
+            for (int i = 0; i < i - 1; i++) {
+                temp2 = temp2->next;
+            }
+            //linked list
+            temp2->next = temp2;
+            iterations++;
+        }
     }
-        //linked list
-     temp2->next =    
-    }
+
+
+
+bool Confirmation(string comment){
+    string x;
+        while(true){
+            cout << comment;
+            getline(cin, x);
+            if (x.at(0) == 'y'){
+                return true;
+            }
+            else if (x.at(0) == 'n'){
+                return false;
+            }   
+        }
 }
 
+int main() {
+    bool runningProgram = true;
+    while (runningProgram) {
+        if(Confirmation("Are you adding a profile? y/n")){
+            DatabaseEnter(iterations);
+        }
+        else{
+            cout << "describe attributes for matchmaking" << endl;
 
-
-int main() { 
-    bool keep = true;
-    int i = 0;
-    while(keep){
-    DatabaseEnter(i);
-       i++;
+            cout << "Matchmaking...";
+        }
     }
+    return 0;
 }
